ce95deaaa0afc439d68c00b410c1f5f592179dbf
Casey Masonek <cmmasonek@gmail.com>
2025-06-11 07:23:45 -0700

    Troubleshooting

--- chem_inventory/settings.py
+++ chem_inventory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$8_sr90yohdws#ixe2+jia1k$43q7(srrxx@5j81oa3m43jmy+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onrender.com']
 
 
 # Application definition
